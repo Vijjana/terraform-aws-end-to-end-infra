bb73f12bafc846222b7410e9e78955c904e2212d
Vijjana <167081992+Vijjana@users.noreply.github.com>
2026-06-02 14:46:03 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,35 @@
+# VPC
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+# Subnet
+resource "aws_subnet" "public" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+}
+
+# Security Group
+resource "aws_security_group" "sg" {
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# EC2
+resource "aws_instance" "web" {
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.public.id
+  vpc_security_group_ids = [aws_security_group.sg.id]
+}
+
+# S3 bucket
+resource "aws_s3_bucket" "bucket" {
+  bucket = "my-terraform-demo-bucket-12345"
+}
